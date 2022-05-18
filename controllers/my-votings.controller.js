892ef650c35f0bb11e71f1bb4609aabbacb0085d
jaeho seo <jaehoseo@jaehoui-MacBookPro.local>
2022-05-18 20:04:59 +0900

    feat: 만든 투표목록 보기 구현

--- controllers/my-votings.controller.js
+++ controllers/my-votings.controller.js
@@ -1,8 +1,16 @@
 const Vote = require("../models/Vote");
 const User = require("../models/User");
+const { dateFormat } = require("../utils/dateFormat");
 
 exports.getMyVoting = async(req, res, next) => {
   const userId = req.user._id;
   
-  const votesId = await User.findById(userId).select("createdVotes").lean();
+  const createdVotes = await Vote.find({ creatorId: userId }).lean();
+  
+  for (const vote of createdVotes) {
+    vote.expiredTime = dateFormat(new Date(vote.expiredTime));
+    vote.isExpired ? vote.isExpired = "투표완료" : vote.isExpired = "투표 진행중";
+  }
+
+  res.render("myVotes", { createdVotes });
 }
